95ce87a4e0db3d5f26fd29e9799afeea2b48c4b2
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-10 19:50:21 +0600

    Created a screen with width = 800 and height = 600 pixels

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 from turtle import Turtle, Screen
+
+screen = Screen()
+screen.screensize(canvwidth=800, canvheight=600)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
