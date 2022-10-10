5904eb1b5104007277c67c4f6dfa8d848b2e1a76
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-10 21:15:57 +0600

    From Ball created ball object

--- ball.py
+++ ball.py
@@ -2,4 +2,7 @@ from turtle import Turtle
 
 class Ball(Turtle):
     def __init__(self):
+        super().__init__()
+        self.color("white")
         self.shape("circle")
+        self.penup()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 import time
-from paddle import Paddle, Ball
+from paddle import Paddle
+from ball import Ball
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -10,7 +12,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0)) #tuple
 l_paddle = Paddle((-350, 0)) #tuple
 # top_paddle = Paddle((200, 0)) #creates another paddle in the given location
-# ball = Ball()
+ball = Ball()
 
 
 
