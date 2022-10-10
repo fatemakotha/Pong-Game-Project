91380c4ed03007f6136f0e2dec6578bde40eb88e
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-10 20:36:37 +0600

    Activated screen.tracer(0) and screen.update()

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PONG GAME")
+screen.tracer(0)
 
 paddle = Turtle("square")
 paddle.color("white")
@@ -11,6 +13,7 @@ paddle.color("white")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(x=350, y=0)
+screen.update()
 
 def go_up():
     new_y = paddle.ycor() + 20
@@ -23,6 +26,10 @@ def go_down():
 screen.listen()
 screen.onkey(go_up, key="Up")
 screen.onkey(go_down, key="Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
