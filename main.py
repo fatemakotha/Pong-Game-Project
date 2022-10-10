31b3ddb01ecdfdb42f069f05d3a3c04f01a8f6ac
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-10 20:57:18 +0600

    Bothe paddles working

--- main.py
+++ main.py
@@ -7,28 +7,19 @@ screen.bgcolor("black")
 screen.title("PONG GAME")
 screen.tracer(0)
 
-r_paddle = Paddle()
-l_paddle = Paddle()
-# paddle = Turtle("square")
-# paddle.color("white")
-# paddle.color("white")
-# paddle.shapesize(stretch_wid=5, stretch_len=1)
-# paddle.penup()
-# paddle.goto(x=350, y=0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor(), new_y)
-#
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor(), new_y)
+
 
 
 screen.listen()
-screen.onkey(paddle.go_up, key="Up")
-screen.onkey(paddle.go_down, key="Down")
+screen.onkey(r_paddle.go_up, key="Up")
+screen.onkey(r_paddle.go_down, key="Down")
+
+screen.onkey(l_paddle.go_up, key="w")
+screen.onkey(l_paddle.go_down, key="s")
 
 game_is_on = True
 while game_is_on:
