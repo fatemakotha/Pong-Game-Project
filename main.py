9bba3ed55a409d1aad883b03e443f99d3ae9003f
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-11 13:32:02 +0600

    Created screen

---
+++ main.py
@@ -0,0 +1,48 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG GAME")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
