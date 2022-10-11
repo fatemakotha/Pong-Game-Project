7c7c794c4eab046b56964aaaf613aede549ddbaa
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-11 13:40:56 +0600

    Paddle moves up and down using screen.listen()

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ paddle.shapesize(stretch_len=1, stretch_wid=5)
 paddle.penup()
 paddle.goto(x=350, y=0)
 
+def go_up():
+    new_y = paddle.ycor() + 10
+    paddle.goto(x=350, y=new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 10
+    paddle.goto(x=350, y=new_y)
+
+
+screen.listen()
+screen.onkey(go_up, key="Up")
+screen.onkey(go_down, key="Down")
 
 
 
